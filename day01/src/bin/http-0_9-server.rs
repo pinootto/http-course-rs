91e5c0e05df5cb92e7d947a3e98ec872463328cd
Giovanni <pino_otto@yahoo.com>
2025-08-09 17:20:52 +0800

    written draft server

---
+++ day01/src/bin/http-0_9-server.rs
@@ -0,0 +1,40 @@
+use std::io::Result;
+
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8091").await?;
+    println!("server listening on {}", listener.local_addr().unwrap());
+    loop {
+        let (mut tcp_stream, _client_socket_address) = listener.accept().await?;
+        // dbg!(&tcp_stream);
+        // dbg!(client_socket_address);
+        let (mut reader, _writer) = tcp_stream.split();
+        let mut buffer: [u8; 200] = [0; 200];
+        let _n = reader.read(&mut buffer).await?;
+        // dbg!(buffer);
+        let mut lines = buffer.lines();
+        if let Some(first_line) = lines.next_line().await? {
+            println!("{first_line}");
+            let mut words = first_line.split_whitespace();
+            if let Some(method) = words.next()
+                && method == "GET"
+            {
+                println!("received a GET request");
+            } else {
+                println!("invalid request");
+                continue;
+            };
+            if let Some(path) = words.next() {
+                println!("path = {}", path);
+            } else {
+                println!("no path");
+                continue;
+            }
+        }
+    }
+}
