1a82cd28d63c80cdb3e3f1d4893f8e1864e81c36
Giovanni <pino_otto@yahoo.com>
2025-08-16 19:33:59 +0800

    code cleaned

--- day02/src/bin/http-1_0-client.rs
+++ day02/src/bin/http-1_0-client.rs
@@ -100,54 +100,5 @@ async fn main() -> Result<()> {
 
     read_response(&mut tcp_stream).await?;
 
-    // // read response
-    // let mut response = String::new();
-    // let _ = tcp_stream.read_to_string(&mut response).await?;
-    // println!("response:");
-    // println!("{response}");
-    // println!();
-    //
-    // let mut line_iter = response.lines();
-    // // first line
-    // println!("first line:");
-    // let first_line = line_iter.next().unwrap();
-    // let mut tokens = first_line.split_whitespace();
-    // if let Some(protocol) = tokens.next() {
-    //     println!("protocol = {}", protocol);
-    // } else {
-    //     panic!();
-    // }
-    // if let Some(status) = tokens.next() {
-    //     println!("status = {}", status)
-    // } else {
-    //     panic!();
-    // }
-    // println!();
-    //
-    // // headers
-    // println!("headers:");
-    // loop {
-    //     let line = line_iter.next();
-    //     if let Some(header) = line {
-    //         if !header.is_empty() {
-    //             println!("header = {}", header);
-    //         } else {
-    //             break;
-    //         }
-    //     } else {
-    //         panic!();
-    //     }
-    // }
-    // println!();
-    //
-    // // body
-    // println!("body:");
-    // // while let Some(line) = line_iter.next() {
-    // //     println!("{}", line);
-    // // }
-    // for line in line_iter {
-    //     println!("{}", line);
-    // }
-    //
     Ok(())
 }
