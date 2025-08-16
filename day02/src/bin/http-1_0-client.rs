9c9d4686dbf18233a42bf5489028d15dc20ac5cf
Giovanni <pino_otto@yahoo.com>
2025-08-16 19:32:25 +0800

    added POST request

--- day02/src/bin/http-1_0-client.rs
+++ day02/src/bin/http-1_0-client.rs
@@ -53,28 +53,50 @@ async fn read_response(tcp_stream: &mut TcpStream) -> Result<()> {
     for line in line_iter {
         println!("{}", line);
     }
+    println!("--------------------------------");
     Ok(())
 }
+
+const ADDRESS_PORT: &str = "localhost:9000";
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // connect to the server
-    let mut tcp_stream = TcpStream::connect("127.0.0.1:9000").await?;
+    let mut tcp_stream = TcpStream::connect(ADDRESS_PORT).await?;
 
-    // send request
+    // send GET request
     tcp_stream
         .write_all(b"GET /headers HTTP/1.0\r\n\r\n")
         .await?;
 
     read_response(&mut tcp_stream).await?;
 
-    // connect to the server
-    let mut tcp_stream = TcpStream::connect("127.0.0.1:9000").await?;
+    // for each request we need to connect to the server again
+    let mut tcp_stream = TcpStream::connect(ADDRESS_PORT).await?;
 
-    // send request
+    // send GET request
     tcp_stream
         .write_all(b"GET /headers HTTP/1.0\r\n\r\n")
         .await?;
+
+    read_response(&mut tcp_stream).await?;
+
+    // for each request we need to connect to the server again
+    let mut tcp_stream = TcpStream::connect(ADDRESS_PORT).await?;
+
+    // send POST request
+    tcp_stream.write_all(b"POST /echo HTTP/1.0\r\n").await?;
+    // headers
+    let request_body = "name=pippo&age=3";
+    let header_1 = format!("content-length: {}\r\n", request_body.len());
+    tcp_stream.write_all(header_1.as_bytes()).await?;
+    tcp_stream
+        .write_all(b"content-type: application/x-www-form-urlencoded\r\n")
+        .await?;
+    // empty line
+    tcp_stream.write_all(b"\r\n").await?;
+    // body
+    tcp_stream.write_all(request_body.as_bytes()).await?;
 
     read_response(&mut tcp_stream).await?;
 
